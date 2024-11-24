11b9e6858e8f642d289b868ef831035cd9655f45
Anselmo Dev <Anselmodev@MacBook-Pro-de-Anselmo.local>
2024-11-24 01:43:25 -0300

    menu att

--- src/Main.java
+++ src/Main.java
@@ -26,113 +26,146 @@ public class Main {
             System.out.println("4 - Fornecedor");
             System.out.println("5 - Transacoes");
             System.out.println("6 - Fechar Menu");
-            int selecao1 = scanner.nextInt();
+            int selecao = scanner.nextInt();
 
-            if (selecao1 == 1) {
-                System.out.println("Produto");
-                System.out.println("1- Adicionar");
-                System.out.println("2- Editar");
-                System.out.println("3- Remover");
-                System.out.println("4- Voltar");
-                int selecao2 = scanner.nextInt();
-                if (selecao2 == 1) {
-                    System.out.println("Tipo: ");
-                    System.out.println("1- Alimento");
-                    System.out.println("2- Bebida");
-                    int selecao3 = scanner.nextInt();
-                    if (selecao3 == 1) {
-                        alimento.adicionarAoEstoque();
-                    } else if (selecao3 == 2) {
-                        bebida.adicionarAoEstoque();
+            switch (selecao) {
+                case 1:
+                    System.out.println("Produto");
+                    System.out.println("1- Adicionar");
+                    System.out.println("2- Editar");
+                    System.out.println("3- Remover");
+                    System.out.println("4- Voltar");
+                    int selecao2 = scanner.nextInt();
+                    switch (selecao2) {
+                        case 1:
+                            System.out.println("Tipo: ");
+                            System.out.println("1- Alimento");
+                            System.out.println("2- Bebida");
+                            int selecao3 = scanner.nextInt();
+                            if (selecao3 == 1) {
+                                alimento.adicionarAoEstoque();
+                            } else if (selecao3 == 2) {
+                                bebida.adicionarAoEstoque();
+                            }
+                            break;
+                        case 2:
+                            estoque.editarProduto();
+                            break;
+                        case 3:
+                            estoque.removerProduto();
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            System.out.println("Opcao invalida!");
                     }
-                } else if (selecao2 == 2) {
-                    estoque.editarProduto();
-                } else if (selecao2 == 3) {
-                    estoque.removerProduto();
-                } else if (selecao2 == 4) {
-                    continue;
-                }
-            }
+                    break;
 
-            else if (selecao1 == 2) {
-                System.out.println("Estoque");
-                System.out.println("1- Ver Estoque");
-                System.out.println("2- Limpar Estoque");
-                System.out.println("3- Voltar");
-                int selecao2 = scanner.nextInt();
-                if (selecao2 == 1) {
-                    estoque.visualizarDados();
-                } else if (selecao2 == 2) {
-                    System.out.println("Informe a senha para limpar o estoque: ");
-                    String senha = scanner.next();
-                    if (senha.equals("1234")) {
-                        estoque.removerDados();
-                        System.out.println("Estoque limpo com sucesso");
+                case 2:
+                    System.out.println("Estoque");
+                    System.out.println("1- Ver Estoque");
+                    System.out.println("2- Limpar Estoque");
+                    System.out.println("3- Voltar");
+                    int selecaoEstoque = scanner.nextInt();
+                    switch (selecaoEstoque) {
+                        case 1:
+                            estoque.visualizarDados();
+                            break;
+                        case 2:
+                            System.out.println("Informe a senha para limpar o estoque: ");
+                            String senha = scanner.next();
+                            if (senha.equals("1234")) {
+                                estoque.removerDados();
+                                System.out.println("Estoque limpo com sucesso");
+                            }
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            System.out.println("Opcao invalida!");
                     }
-                } else if (selecao2 == 3) {
-                    continue;
-                }
-            }
+                    break;
 
-            else if (selecao1 == 3) {
-                System.out.println("Funcionario");
-                System.out.println("1- Cadastrar Funcionario");
-                System.out.println("2- Remover Funcionario");
-                System.out.println("3- Ver funcionarios");
-                System.out.println("4- Voltar");
-                int selecao2 = scanner.nextInt();
-                if (selecao2 == 1) {
-                    funcionario.adionarPessoa();
-                } else if (selecao2 == 2) {
-                    funcionario.removerPessoa();
-                } else if (selecao2 == 3) {
-                    funcionario.visualizarDados();
-                } else if (selecao2 == 4) {
-                    continue;
-                }
-            }
-
-            else if (selecao1 == 4) {
-                System.out.println("Fornecedor");
-                System.out.println("1- Cadastrar Fornecedor");
-                System.out.println("2- Remover Fornecedor");
-                System.out.println("3- Ver funcionarios");
-                System.out.println("4- Voltar");
-                int selecao2 = scanner.nextInt();
-                if (selecao2 == 1) {
-                    fornecedor.adionarPessoa();
-                } else if (selecao2 == 2) {
-                    fornecedor.removerPessoa();
-                } else if (selecao2 == 3) {
-                    fornecedor.visualizarDados();
-                } else if (selecao2 == 4) {
-                    continue;
-                }
-            }
-
-            else if (selecao1 == 5) {
-                System.out.println("Transacoes");
-                System.out.println("1- Ver Transacoes");
-                System.out.println("2- Limpar Transacoes");
-                System.out.println("3- Voltar");
-                int selecao2 = scanner.nextInt();
-                if (selecao2 == 1) {
-                    entrada.visualizarDados();
-                } else if (selecao2 == 2) {
-                    System.out.println("Informe a senha para limpar as Transacoes: ");
-                    String senha = scanner.next();
-                    if (senha.equals("abcd")) {
-                        entrada.removerDados();
-                        System.out.println("Transacoes limpa com sucesso");
-                    } else if (selecao2 == 3) {
-                        continue;
+                case 3:
+                    System.out.println("Funcionario");
+                    System.out.println("1- Cadastrar Funcionario");
+                    System.out.println("2- Remover Funcionario");
+                    System.out.println("3- Ver funcionarios");
+                    System.out.println("4- Voltar");
+                    int selecaoFuncionario = scanner.nextInt();
+                    switch (selecaoFuncionario) {
+                        case 1:
+                            funcionario.adionarPessoa();
+                            break;
+                        case 2:
+                            funcionario.removerPessoa();
+                            break;
+                        case 3:
+                            funcionario.visualizarDados();
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            System.out.println("Opcao invalida!");
                     }
-                }
-            }
+                    break;
 
-            if (selecao1 == 6) {
-                menu = false;
-                System.out.println("Programa Finalizado!");
+                case 4:
+                    System.out.println("Fornecedor");
+                    System.out.println("1- Cadastrar Fornecedor");
+                    System.out.println("2- Remover Fornecedor");
+                    System.out.println("3- Ver fornecedores");
+                    System.out.println("4- Voltar");
+                    int selecaoFornecedor = scanner.nextInt();
+                    switch (selecaoFornecedor) {
+                        case 1:
+                            fornecedor.adionarPessoa();
+                            break;
+                        case 2:
+                            fornecedor.removerPessoa();
+                            break;
+                        case 3:
+                            fornecedor.visualizarDados();
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            System.out.println("Opcao invalida!");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Transacoes");
+                    System.out.println("1- Ver Transacoes");
+                    System.out.println("2- Limpar Transacoes");
+                    System.out.println("3- Voltar");
+                    int selecaoTransacoes = scanner.nextInt();
+                    switch (selecaoTransacoes) {
+                        case 1:
+                            entrada.visualizarDados();
+                            break;
+                        case 2:
+                            System.out.println("Informe a senha para limpar as Transacoes: ");
+                            String senhaTransacoes = scanner.next();
+                            if (senhaTransacoes.equals("abcd")) {
+                                entrada.removerDados();
+                                System.out.println("Transacoes limpa com sucesso");
+                            }
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            System.out.println("Opcao invalida!");
+                    }
+                    break;
+
+                case 6:
+                    menu = false;
+                    System.out.println("Programa Finalizado!");
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida!");
             }
         }
 
